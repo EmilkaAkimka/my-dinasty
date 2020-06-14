d2f9626f4a761c2728a4cb879969fc6a1666f17c
EmilkaAkimka <Hirurghacker@gmail.com>
2020-06-14 12:36:11 -0700

    add a main many (dont works)

--- dynasty.py
+++ dynasty.py
@@ -1,2 +1,15 @@
-print("Здравствуйте Арслан Тарланович, это собщение на тот случай если вы решили проверить начало")
-print("посмотрим сработает ли")
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200,200,300,300)
+    win.setWindowTitle("Tech With Timi")
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()
